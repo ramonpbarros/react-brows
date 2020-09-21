56dea2e0e2f9c093cd811ffa3a68d12efe6223eb
Sorrab <ramonpbarros@gmail.com>
2020-09-21 11:10:50 -0700

    Updates

---
+++ precache-manifest.7e06f8397412d3f61b29f7d8ff5a721e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a432c98275a8353e4a65ac911d268d",
+    "url": "/react-brows/index.html"
+  },
+  {
+    "revision": "2380f6118cc79c403a40",
+    "url": "/react-brows/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8179ee1ba757f7f79901",
+    "url": "/react-brows/static/js/2.23661bb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-brows/static/js/2.23661bb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2380f6118cc79c403a40",
+    "url": "/react-brows/static/js/main.e039be46.chunk.js"
+  },
+  {
+    "revision": "fd18acabb4a609646a1f",
+    "url": "/react-brows/static/js/runtime-main.a2c31713.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-brows/static/media/logo.5d5d9eef.svg"
+  }
+]);

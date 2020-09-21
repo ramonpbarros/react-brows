693b1f459b89504a61cd4b88b9cc14093b357aa4
Sorrab <ramonpbarros@gmail.com>
2020-09-21 11:09:52 -0700

    deploy react app to gh

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Gabriella Barros
         </a>
       </header>
     </div>
